acfcfd9a634d8c35ddf6371b7b12acd774c68a63
notashwinii <ashwinii.subedi@gmail.com>
2026-06-26 10:42:09 +0545

    chore(config): update WhatsApp contact

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,10 +1,10 @@
-export const JAWAFDEHI_WHATSAPP_NUMBER = "+1 (206) 530-9098";
+export const JAWAFDEHI_WHATSAPP_NUMBER = "+977 9749226442";
 export const JAWAFDEHI_EMAIL = "inquiry@jawafdehi.org";
 
 export const JAWAFDEHI_SOCIALS = {
   facebook: "https://www.facebook.com/jawafdehi",
   youtube: "https://www.youtube.com/@Jawafdehi",
   linkedin: "https://www.linkedin.com/company/jawafdehi-initiative",
-  whatsapp: "https://api.whatsapp.com/send?phone=12065309098",
+  whatsapp: "https://api.whatsapp.com/send?phone=9779749226442",
   linktree: "https://linktr.ee/jawafdehi",
 };
